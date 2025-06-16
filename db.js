2bf8df8304e271d04c3adf806a296c801022b077
harshit <upadhyayharshit05@gmail.com>
2025-06-16 09:38:57 +0530

    u

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
-async function connectDB() {
+const { default: mongoose } = require("mongoose");
+
+const connectDB=async()=> {
    try{
 
        await mongoose.connect(
-           'mongodb+srv://upadhyayharshit05:7kBDgfDuNbc0cfIQ@cluster0.jujvf30.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-           { useNewUrlParser: true, useUnifiedTopology: true }
+           'mongodb+srv://upadhyayharshit05:7kBDgfDuNbc0cfIQ@cluster0.jujvf30.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' 
         );
-    }
-    catch(err){   
         console.log("Connected to MongoDB");
     }
+    catch(err){  
+        console.log("Error while connecting db",err) 
+    }
   }
-
 module.exports=connectDB
